028baacd64e79ea765840bd966f1390f029b84de
Kim H. Thorsen <khthorse@gmail.com>
2026-03-27 16:30:15 +0100

    Added color picker to qr code generator

--- QR_gen.py
+++ QR_gen.py
@@ -3,20 +3,25 @@ import qrcode
 from tkinter import *
 from tkinter import messagebox, colorchooser, filedialog
 
-background_color = 'DarkTurquoise'
-qr_fill_color = "black"
-qr_back_color = "white"
+from color_picker import pick_color
+
+background_color = "#292929"
+frame_bg_color = "#1F1F1F"
+text_color = "#B3B3B3"
+qr_fill_color = "#000000"
+qr_back_color = "#ffffff"
+
 
 # Creating the window
 win = Tk()
-win.title('QR Code Generator')
+win.title('Kims QR Code Generator')
 win.geometry('650x750')
 win.config(bg=background_color)
 
 
 def choose_fill_color():
     global qr_fill_color
-    color = colorchooser.askcolor(title="Choose QR color")[1]
+    color = pick_color(win, initial_color=qr_fill_color, title="Choose QR color")
     if color:
         qr_fill_color = color
         fillColorLabel.config(text=f"Selected QR color: {qr_fill_color}")
@@ -24,7 +29,7 @@ def choose_fill_color():
 
 def choose_back_color():
     global qr_back_color
-    color = colorchooser.askcolor(title="Choose background color")[1]
+    color = pick_color(win, initial_color=qr_back_color, title="Choose background color")
     if color:
         qr_back_color = color
         backColorLabel.config(text=f"Selected background color: {qr_back_color}")
@@ -49,7 +54,7 @@ def generateCode():
             return
 
         if not save_location:
-            messagebox.showerror("Error", "Please enter a save location.")
+            messagebox.showerror("Error", "Please choose where to save the qr code.")
             return
 
         if not qr_name:
@@ -57,7 +62,7 @@ def generateCode():
             return
 
         if not qr_size:
-            qr_size = 1
+            qr_size = 5
         elif not qr_size.isdigit():
             messagebox.showerror("Error", "Size must be a number from 1 to 40.")
             return
@@ -72,7 +77,7 @@ def generateCode():
             messagebox.showerror("Error", "The folder does not exist.")
             return
 
-        qr = qrcode.QRCode(version=qr_size, box_size=10, border=5)
+        qr = qrcode.QRCode(version=qr_size, box_size=10, border=1)
         qr.add_data(qr_text)
         qr.make(fit=True)
 
@@ -81,22 +86,22 @@ def generateCode():
         file_path = os.path.join(save_location, f"{qr_name}.png")
         img.save(file_path)
 
-        messagebox.showinfo("QR Code Generator", f"QR Code saved successfully!\n\nSaved as:\n{file_path}")
+        messagebox.showinfo("Kims QR Code Generator", f"QR Code saved successfully!\n\nSaved as:\n{file_path}")
 
     except Exception as e:
         messagebox.showerror("Error", f"Something went wrong:\n{e}")
 
 
-headingFrame = Frame(win, bg="azure", bd=5)
+headingFrame = Frame(win, bg=background_color, bd=5)
 headingFrame.place(relx=0.15, rely=0.05, relwidth=0.7, relheight=0.1)
 
-headingLabel = Label(headingFrame, text="Generate QR Code", bg='azure', font=('Times', 20, 'bold'))
+headingLabel = Label(headingFrame, text="Kims QR Code generator", bg=frame_bg_color, fg=text_color, font=('FiraMono', 20, 'bold'))
 headingLabel.place(relx=0, rely=0, relwidth=1, relheight=1)
 
 Frame1 = Frame(win, bg=background_color)
 Frame1.place(relx=0.1, rely=0.15, relwidth=0.7, relheight=0.15)
 
-label1 = Label(Frame1, text="Enter the text/URL: ", bg=background_color, fg='azure', font=('FiraMono', 13, 'bold'))
+label1 = Label(Frame1, text="Enter the text/URL: ", bg=background_color, fg=text_color, font=('FiraMono', 13, 'bold'))
 label1.place(relx=0.05, rely=0.1, relheight=0.15)
 
 text_entry = Entry(Frame1, font=('Century 12'))
@@ -105,7 +110,7 @@ text_entry.place(relx=0.05, rely=0.35, relwidth=1, relheight=0.25)
 Frame2 = Frame(win, bg=background_color)
 Frame2.place(relx=0.1, rely=0.30, relwidth=0.7, relheight=0.15)
 
-label2 = Label(Frame2, text="Enter the location to save the QR Code: ", bg=background_color, fg='azure', font=('FiraMono', 13, 'bold'))
+label2 = Label(Frame2, text="Enter the location to save the QR Code: ", bg=background_color, fg=text_color, font=('FiraMono', 13, 'bold'))
 label2.place(relx=0.05, rely=0.1, relheight=0.15)
 
 loc_entry = Entry(Frame2, font=('Century 12'))
@@ -117,7 +122,7 @@ browseButton.place(relx=0.82, rely=0.35, relwidth=0.18, relheight=0.25)
 Frame3 = Frame(win, bg=background_color)
 Frame3.place(relx=0.1, rely=0.45, relwidth=0.7, relheight=0.15)
 
-label3 = Label(Frame3, text="Enter the name of the QR Code: ", bg=background_color, fg='azure', font=('FiraMono', 13, 'bold'))
+label3 = Label(Frame3, text="Enter the name of the QR Code: ", bg=background_color, fg=text_color, font=('FiraMono', 13, 'bold'))
 label3.place(relx=0.05, rely=0.1, relheight=0.15)
 
 name_entry = Entry(Frame3, font=('Century 12'))
@@ -126,7 +131,7 @@ name_entry.place(relx=0.05, rely=0.35, relwidth=1, relheight=0.25)
 Frame4 = Frame(win, bg=background_color)
 Frame4.place(relx=0.1, rely=0.60, relwidth=0.7, relheight=0.12)
 
-label4 = Label(Frame4, text="Enter the size from 1 to 40 with 1 being 21x21: ", bg=background_color, fg='azure', font=('FiraMono', 13, 'bold'))
+label4 = Label(Frame4, text="Enter the size from 1 to 40 with 1 being 21x21: ", bg=background_color, fg=text_color, font=('FiraMono', 13, 'bold'))
 label4.place(relx=0.05, rely=0.1, relheight=0.2)
 
 size_entry = Entry(Frame4, font=('Century 12'))
@@ -142,10 +147,10 @@ fillColorButton.place(relx=0.05, rely=0.1, relwidth=0.4, relheight=0.3)
 backColorButton = Button(Frame5, text='Choose background color', font=('FiraMono', 11), command=choose_back_color)
 backColorButton.place(relx=0.55, rely=0.1, relwidth=0.4, relheight=0.3)
 
-fillColorLabel = Label(Frame5, text=f"Selected QR color: {qr_fill_color}", bg=background_color, fg='azure', font=('FiraMono', 10))
+fillColorLabel = Label(Frame5, text=f"Selected QR color: {qr_fill_color}", bg=background_color, fg=text_color, font=('FiraMono', 10))
 fillColorLabel.place(relx=0.05, rely=0.5)
 
-backColorLabel = Label(Frame5, text=f"Selected background color: {qr_back_color}", bg=background_color, fg='azure', font=('FiraMono', 10))
+backColorLabel = Label(Frame5, text=f"Selected background color: {qr_back_color}", bg=background_color, fg=text_color, font=('FiraMono', 10))
 backColorLabel.place(relx=0.05, rely=0.75)
 
 button = Button(win, text='Generate Code', font=('FiraMono', 15, 'normal'), command=generateCode)
